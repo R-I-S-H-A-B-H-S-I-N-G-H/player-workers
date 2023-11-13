023da2c63320b2d9a68944c0c16c3e7752e574e4
Rishabh Singh <rishabhsingh2305@gmail.com>
2023-11-13 10:04:46 +0000

    removed logs

--- Api/S3Api.js
+++ Api/S3Api.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 
 exports.uploadToS3 = async (props) => {
 	const BASE_URL = process.env.S3BASE_URL;
-	console.log("S3 URL :: ", BASE_URL);
 	const { fileBody, fileName } = props;
 	try {
 		const resp = await axios.post(BASE_URL + "/upload", {
