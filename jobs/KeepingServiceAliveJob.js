83c1ad1115784ca13da871adc307ff60e5393322
rishabh <rishabhsingh2305@gmail.com>
2023-11-23 21:28:02 +0530

    increased the ksa time

--- jobs/KeepingServiceAliveJob.js
+++ jobs/KeepingServiceAliveJob.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const schedule = require("node-schedule");
 
 exports.keepServiceAlive = () =>
-	schedule.scheduleJob("*/2 * * * * *", async () => {
+	schedule.scheduleJob("* */14 * * * *", async () => {
 		console.log("----PING JOB-----");
 		const pingURL = process.env.PING_URL;
 		const res = await axios.get(pingURL);
